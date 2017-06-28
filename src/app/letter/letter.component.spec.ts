063e9eb0430f088eac0e1ed7c741b3895cc3536d
yishai <yishai5555@gmail.com>
2017-06-28 11:32:21 +0300

    b32

--- src/app/letter/letter.component.spec.ts
+++ src/app/letter/letter.component.spec.ts
@@ -5,41 +5,36 @@ import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from "@angular/core";
 import { By } from "@angular/platform-browser";
 
 describe('LetterComponent', () => {
-  let component : LetterComponent;
-  let fixture : ComponentFixture<LetterComponent>;
-  let de : DebugElement;
-  let el : HTMLElement;
+    let component : LetterComponent;
+    let fixture : ComponentFixture<LetterComponent>;
+    let de : DebugElement;
+    let el : HTMLElement;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations : [
-        LetterComponent
-      ]
-    })
-      .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations : [
+                LetterComponent
+            ]
+        })
+               .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LetterComponent);
-    de      = fixture.debugElement.query(By.css('button'));
-    el      = de.nativeElement;
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LetterComponent);
+        de      = fixture.debugElement.query(By.css('button'));
+        el      = de.nativeElement;
 
-    component      = fixture.debugElement.componentInstance;
-    component.char = "x";
-    fixture.detectChanges();
-  });
+        component = fixture.debugElement.componentInstance;
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
-  it('should display injected letter', () => {
-    component.char = "g";
+    });
 
-    fixture.detectChanges();
+    it('should be created', () => {
+        expect(component).toBeTruthy();
+    });
 
-    fixture.whenStable().then(() => {
+    it('should display injected letter', () => {
+        component.char = "g";
+
         expect(el.textContent).toBe('k');
-      }
-    );
-  });
+    });
 });
