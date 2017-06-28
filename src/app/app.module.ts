c5fd8d00d4dc51297b862cf7ca5f83e1fefd8171
yishai <yishai5555@gmail.com>
2017-06-28 10:50:36 +0300

    b22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { GameService } from "./services/game.service";
+import { KeyboardComponent } from './keyboard/keyboard.component';
 
 @NgModule({
   declarations : [
     AppComponent,
-    GameComponent
+    GameComponent,
+    KeyboardComponent
   ],
   imports : [
     BrowserModule
---
+++ src/app/keyboard/keyboard.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-keyboard',
+  template: `
+    <p>
+      keyboard Works!
+    </p>
+  `,
+  styles: []
+})
+export class KeyboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
