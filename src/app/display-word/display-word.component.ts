1c6ac43fa2adaa3d18ffc4af31cd58cb5586cc08
yishaiz <yishai5555@gmail.com>
2017-06-30 16:24:04 +0300

    b68

--- src/app/display-word/display-word.component.ts
+++ src/app/display-word/display-word.component.ts
@@ -3,11 +3,15 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
     selector : 'app-display-word',
     template : `
-        <h2>
+        <h2 class="word-letters">
             {{word}}
         </h2>
     `,
-    styles : []
+    styles : [`
+        .word-letters{
+            letter-spacing: 22px;
+        }
+    `]
 })
 export class DisplayWordComponent implements OnInit {
 
